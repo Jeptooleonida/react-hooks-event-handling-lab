ff5fc0f52275b538bdcb9aaaf1b0cef9a6a85af3
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-07-14 23:52:09 +0300

    complete test

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
+
+
 // Code Keypad Component Here
+
+function Keypad(){
+    return (
+      <input 
+        type= "password"
+        placeholder = "Password"
+        onChange={ () => console.log(`Entering password...`)} 
+
+      />
+    )
+  }
+  
+  export default Keypad;
